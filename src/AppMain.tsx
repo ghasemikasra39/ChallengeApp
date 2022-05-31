35f11f0fce1f6d380251d787ff8d30decd776190
Kasra <ghasemikasra39@yahoo.com>
2022-05-31 18:59:56 +0200

    Add loading indicator

--- src/AppMain.tsx
+++ src/AppMain.tsx
@@ -1,13 +1,28 @@
 import React, {FC} from 'react';
+import {View, ActivityIndicator, StyleSheet} from 'react-native';
 import OnboardingScreen from './screens/OnboardingScreen';
 import {useGetDataQuery} from './store/slices/OnboardingSlice';
+import {AppColors} from './globals/AppColors';
 
 const AppMain: FC = () => {
   const {data, isLoading} = useGetDataQuery(null);
 
-  if (isLoading) return <></>;
+  if (isLoading)
+    return (
+      <View style={styles.activityIndicator}>
+        <ActivityIndicator size="large" color={AppColors.blue} />
+      </View>
+    );
 
   return <OnboardingScreen data={data.data} />;
 };
 
 export default AppMain;
+
+const styles = StyleSheet.create({
+  activityIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
