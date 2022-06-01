6d65d012ba31c068113d8c767dda4e274158f565
Kasra <ghasemikasra39@yahoo.com>
2022-06-01 10:58:21 +0200

    Add missing FC type on App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import {Provider} from 'react-redux';
 import {store} from './src/store/Store';
 
-const App = () => {
+const App: FC = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
